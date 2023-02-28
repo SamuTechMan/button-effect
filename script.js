be688d9e3bb4087bd1ae15b1dee4239d6b8c0470
SamuTechMan <117929531+SamuTechMan@users.noreply.github.com>
2023-02-28 17:12:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+/*let a = 7;
+let b = 3;
+b +=a;
+console.log(b);
+b -=a;
+console.log(b);
+b *=a;
+console.log(b);
+b /=a;
+console.log(b);
+b %=2;
+console.log(b);
+function compras(trabalho1, trabalho2) {
+  const comprarSorvete = trabalho1 || trabalho2;
+  const comprarTv50 = trabalho1 && trabalho2;
+  const comprarTv32 = trabalho1 != trabalho2;
+  const manterSaudavel = !comprarSorvete;
+  return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel };
+}
+console.log(compras(false, false));*/
+
+console.log('(01)', '1' == 1)
+console.log('(02)', '1' === 1)
+console.log('(03)', '1' != 1)
+console.log('(04)', '3' !== 3)
+console.log('(05)', 5 > 1)
+console.log('(06)', 5 < 1)
+console.log('(07)', 5 >= 1)
+console.log('(08)', 5 <= 1)
